1434cf431ec84d56f7f87577637f8af68aa8eadb
maxwellsgomes <maxwellsgomes@hotmail.com>
2019-11-12 21:02:29 -0300

    Mail Configuration

---
+++ src/config/mail.js
@@ -0,0 +1,14 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '9aad6256b346d3',
+    pass: '0446f47b47682c',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
+
+/* Mailtrap - Somente para ambiente DEV */
